c947e1d2d05c0835533dd1f17e9b5290aadb2671
Mohammed Salah <27531732+MohammedSalahCode@users.noreply.github.com>
2026-04-11 17:36:03 +0300

    Initial project setup - StudentManagement API with Client application

---
+++ StudentManagement.API/Controllers/StudentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.API.DataSimulation;
+using StudentManagement.API.Models;
+
+
+namespace StudentManagement.API.Controllers
+{
+    public class StudentsController : ControllerBase
+    {
+    }
+}
